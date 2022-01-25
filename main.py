b49c24c297937e8aee7a3b95b56ff36d0369ba43
Cornelius Jermaine Hightower <75323116+Cjhightower38@users.noreply.github.com>
2022-01-25 20:45:05 +0000

    add better styling for the variable.

--- main.py
+++ main.py
@@ -4,9 +4,9 @@
 import random
 
 # From the random module used the randint() and a simple if statement to determine the coin flip.
-c = random.randint(0,1)
+heads_or_tails = random.randint(0,1)
 
-if c == 1:
+if heads_or_tails == 1:
     print('Heads')
 else:
     print('Tails')
