4b8fe889f1d08254f8eeb77bd8430c8b038b56d5
Cornelius Jermaine Hightower <75323116+Cjhightower38@users.noreply.github.com>
2022-01-25 20:43:55 +0000

    add documentation on for the code works.

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 #Write your code below this line 👇
 #Hint: Remember to import the random module first. 🎲
-
+# Import the random module
 import random
 
+# From the random module used the randint() and a simple if statement to determine the coin flip.
 c = random.randint(0,1)
+
 if c == 1:
     print('Heads')
 else:
